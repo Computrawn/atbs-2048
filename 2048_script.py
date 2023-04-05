3fd767f1eeca534b3946ef3ac05204dfe7fd7523
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-05 15:29:27 -0500

    Split up functionality.

--- 2048_script.py
+++ 2048_script.py
@@ -5,6 +5,8 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+PAUSE = 0.001
+
 
 def open_site(site):
     """Opens 2048 game site."""
@@ -15,22 +17,35 @@ def open_site(site):
 
 
 def play_2048(browser):
-    """Sends up, right, down left commands."""
+    """Plays game by sending loop of direction commands."""
     push_button = browser.find_element_by_class_name("container")
     for _ in range(loop_moves):
-        push_button.send_keys(Keys.UP)
-        time.sleep(pause_time)
-        push_button.send_keys(Keys.RIGHT)
-        time.sleep(pause_time)
-        push_button.send_keys(Keys.DOWN)
-        time.sleep(pause_time)
-        push_button.send_keys(Keys.LEFT)
-        time.sleep(pause_time)
+        push_button.send_keys(Keys.UP, Keys.RIGHT, Keys.DOWN, Keys.LEFT)
+        time.sleep(PAUSE)
+    return browser
+
+
+def final_score(browser):
+    """Calculates final score and closes browser."""
+    score_field = browser.find_element_by_class_name("score-container")
+    score = score_field.text
+    split_score = score.split("+")
+    browser.close()
+    return split_score[0]
 
 
 website = "https://play2048.co"
-pause_time = 0.01
 loop_moves = int(input("How times would you like the loop to execute? "))
 
 site_open = open_site(website)
-play_2048(site_open)
+get_score = play_2048(site_open)
+print(f"You scored {final_score(get_score)}.")
+
+while True:
+    play_again = input("Would you like to play again? ").lower()
+    if play_again == "yes":
+        site_open = open_site(website)
+        get_score = play_2048(site_open)
+        print(f"You scored {final_score(get_score)}.")
+    else:
+        break
