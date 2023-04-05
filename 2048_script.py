4f0e4639693506e83aaa89d8ae70d40d436cc792
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-05 14:02:51 -0500

    Created user-defined loop; needs improvement.

--- 2048_script.py
+++ 2048_script.py
@@ -10,26 +10,27 @@ def open_site(site):
     """Opens 2048 game site."""
     browser = webdriver.Safari()
     browser.get(site)
-    time.sleep(pause_time)
+    browser.set_window_size(1300, 1300)
     return browser
 
 
 def play_2048(browser):
     """Sends up, right, down left commands."""
     push_button = browser.find_element_by_class_name("container")
-    push_button.send_keys(Keys.UP)
-    time.sleep(pause_time)
-    push_button.send_keys(Keys.RIGHT)
-    time.sleep(pause_time)
-    push_button.send_keys(Keys.DOWN)
-    time.sleep(pause_time)
-    push_button.send_keys(Keys.LEFT)
-    time.sleep(pause_time)
+    for _ in range(loop_moves):
+        push_button.send_keys(Keys.UP)
+        time.sleep(pause_time)
+        push_button.send_keys(Keys.RIGHT)
+        time.sleep(pause_time)
+        push_button.send_keys(Keys.DOWN)
+        time.sleep(pause_time)
+        push_button.send_keys(Keys.LEFT)
+        time.sleep(pause_time)
 
 
 website = "https://play2048.co"
-pause_time = 1
-# play_moves = int(input("How moves would you like to make? "))
+pause_time = 0.01
+loop_moves = int(input("How times would you like the loop to execute? "))
 
 site_open = open_site(website)
 play_2048(site_open)
