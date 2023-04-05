add69eed99bcc0ca6ef568340858b927b80de692
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-05 12:55:50 -0500

    Initial commit.

---
+++ 2048_script.py
@@ -0,0 +1,13 @@
+#! python3
+# 2048_script.py — An exercise in web automation.
+
+import time
+from selenium import webdriver
+
+site = "https://play2048.co"
+browser = webdriver.Safari()
+browser.get(site)
+
+
+def play_2048():
+    pass
