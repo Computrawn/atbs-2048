813ffb862b7bd9e9533144f3109d224037b7299e
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-04-05 13:51:49 -0500

    Established move loop by identifying container class name on site.

--- 2048_script.py
+++ 2048_script.py
@@ -3,11 +3,33 @@
 
 import time
 from selenium import webdriver
-
-site = "https://play2048.co"
-browser = webdriver.Safari()
-browser.get(site)
+from selenium.webdriver.common.keys import Keys
 
 
-def play_2048():
-    pass
+def open_site(site):
+    """Opens 2048 game site."""
+    browser = webdriver.Safari()
+    browser.get(site)
+    time.sleep(pause_time)
+    return browser
+
+
+def play_2048(browser):
+    """Sends up, right, down left commands."""
+    push_button = browser.find_element_by_class_name("container")
+    push_button.send_keys(Keys.UP)
+    time.sleep(pause_time)
+    push_button.send_keys(Keys.RIGHT)
+    time.sleep(pause_time)
+    push_button.send_keys(Keys.DOWN)
+    time.sleep(pause_time)
+    push_button.send_keys(Keys.LEFT)
+    time.sleep(pause_time)
+
+
+website = "https://play2048.co"
+pause_time = 1
+# play_moves = int(input("How moves would you like to make? "))
+
+site_open = open_site(website)
+play_2048(site_open)
