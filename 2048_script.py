8ca4de01d7d015df7b71222e620bc7ac267dc56a
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-20 20:32:05 -0500

    Update code to fix changes in Selenium.

--- 2048_script.py
+++ 2048_script.py
@@ -4,6 +4,8 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 PAUSE = 0.001
 
@@ -18,7 +20,7 @@ def open_site(site):
 
 def play_2048(browser):
     """Plays game by sending loop of direction commands."""
-    push_button = browser.find_element_by_class_name("container")
+    push_button = browser.find_element(By.CLASS_NAME, "container")
     for _ in range(loop_moves):
         push_button.send_keys(Keys.UP, Keys.RIGHT, Keys.DOWN, Keys.LEFT)
         time.sleep(PAUSE)
@@ -27,7 +29,7 @@ def play_2048(browser):
 
 def final_score(browser):
     """Calculates final score and closes browser."""
-    score_field = browser.find_element_by_class_name("score-container")
+    score_field = browser.find_element(By.CLASS_NAME, "score-container")
     score = score_field.text
     split_score = score.split("+")
     # time.sleep(10)
