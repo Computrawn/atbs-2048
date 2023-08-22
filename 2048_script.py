c4d8afcda2ded257a631e4643c398a80791aeded
Kvasir-reborn <123034787+Kvasir-reborn@users.noreply.github.com>
2023-08-21 19:03:16 -0500

    Make main function.

--- 2048_script.py
+++ 2048_script.py
@@ -1,10 +1,19 @@
-#! python3
+#!/usr/bin/env python3
 # 2048_script.py — An exercise in web automation.
+# For more information, see README.md
 
+import logging
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="logging.txt",
+    format="%(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)  # Note out to enable logging.
 
 
 PAUSE = 0.001
@@ -18,7 +27,7 @@ def open_site(site):
     return browser
 
 
-def play_2048(browser):
+def play_2048(browser, loop_moves):
     """Plays game by sending loop of direction commands."""
     push_button = browser.find_element(By.CLASS_NAME, "container")
     for _ in range(loop_moves):
@@ -37,18 +46,23 @@ def final_score(browser):
     return split_score[0]
 
 
-website = "https://play2048.co"
-loop_moves = int(input("How times would you like the loop to execute? "))
+def main():
+    website = "https://play2048.co"
+    loop_moves = int(input("How times would you like the loop to execute? "))
 
-site_open = open_site(website)
-get_score = play_2048(site_open)
-print(f"You scored {final_score(get_score)}.")
+    site_open = open_site(website)
+    get_score = play_2048(site_open, loop_moves)
+    print(f"You scored {final_score(get_score)}.")
 
-while True:
-    play_again = input("Would you like to play again? ").lower()
-    if play_again == "yes":
-        site_open = open_site(website)
-        get_score = play_2048(site_open)
-        print(f"You scored {final_score(get_score)}.")
-    else:
-        break
+    while True:
+        play_again = input("Would you like to play again? ").lower()
+        if play_again == "yes":
+            site_open = open_site(website)
+            get_score = play_2048(site_open, loop_moves)
+            print(f"You scored {final_score(get_score)}.")
+        else:
+            break
+
+
+if __name__ == "__main__":
+    main()
